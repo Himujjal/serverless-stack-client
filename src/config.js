e92e3a43da949b43852dcb655df3d238d09df2bb
Himujjal <upadhyayahimujjal@gmail.com>
2020-05-13 13:48:16 +0530

    final changes to client

--- src/config.js
+++ src/config.js
@@ -1,46 +1,44 @@
 const dev = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: "pk_test_MV32hRB0WlDixj8tzwU3FKEN003XOZAqWq",
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-qvdowkch2uj1"
+    REGION: "ap-south-1",
+    BUCKET: "notes-app-uploads-himu",
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/dev"
+    REGION: "ap-south-1",
+    URL: "https://jyt1csj57i.execute-api.ap-south-1.amazonaws.com/dev",
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_f8l0OHPtp",
-    APP_CLIENT_ID: "5r5o292r1n7s2vgje5u5c7vpq0",
-    IDENTITY_POOL_ID: "us-east-1:b718098b-fe72-410b-b4c6-0750f9135672"
-  }
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_3u4IG0u1d",
+    APP_CLIENT_ID: "52bm38e39443ala2laibqtakea",
+    IDENTITY_POOL_ID: "ap-south-1:caf36977-2df8-49a8-b8a5-336e4cc42f4d",
+  },
 };
 
 const prod = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: "pk_test_MV32hRB0WlDixj8tzwU3FKEN003XOZAqWq",
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1v9w7kkxnznb7"
+    REGION: "ap-south-1",
+    BUCKET: "notes-app-uploads-himu",
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://api.serverless-stack.seed-demo.club/prod"
+    REGION: "ap-south-1",
+    URL: "https://jyt1csj57i.execute-api.ap-south-1.amazonaws.com/prod",
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_mLbfKylhm",
-    APP_CLIENT_ID: "mli2vaupiq3ga29m4698m6mrl",
-    IDENTITY_POOL_ID: "us-east-1:4e377eff-0617-4098-b218-673490ffab8d"
-  }
+    REGION: "ap-south-1",
+    USER_POOL_ID: "ap-south-1_3u4IG0u1d",
+    APP_CLIENT_ID: "52bm38e39443ala2laibqtakea",
+    IDENTITY_POOL_ID: "ap-south-1:caf36977-2df8-49a8-b8a5-336e4cc42f4d",
+  },
 };
 
 // Default to dev if not set
-const config = process.env.REACT_APP_STAGE === 'prod'
-  ? prod
-  : dev;
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
 
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  ...config
+  ...config,
 };
